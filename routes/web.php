5f06a762078d0bb95e6f0ab9581a98fabd1adebd
Stanisław Froń <stasiu.fron2003@gmail.com>
2024-05-19 14:32:34 +0200

    Structure rebuild

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GoogleDriveController;
 
-Route::get('/', [GoogleDriveController::class, 'displayTest']);
+Route::get('/test/{google_document_number}/{number_of_answers}/{type}', [TestController::class, 'show']);
+
+//http://localhost:8000/test/10I3OiNUYWvwXBGNnYlUWb7YQ46rmTQRK8YWcKH7_wyY/4/checkbox
